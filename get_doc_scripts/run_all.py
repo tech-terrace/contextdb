7b22580a577c524ade04248f6dbd4cd646d34743
Oleksii Avdieiev <oleksii@tech-terrace.org>
2024-05-15 17:20:12 +0200

    Remove print statements from run_all.py

--- get_doc_scripts/run_all.py
+++ get_doc_scripts/run_all.py
@@ -3,8 +3,6 @@ from get_doc_scripts import tools
 
 # Iterate over all modules in the 'tools' package
 for importer, modname, ispkg in pkgutil.iter_modules(tools.__path__, tools.__name__ + '.'):
-    print("Found submodule %s (is a package: %s)" % (modname, ispkg))
     module = __import__(modname, fromlist="dummy")
     if hasattr(module, 'scraper'):
-        print(f"Running scraper for {modname}")
         module.scraper.run()
