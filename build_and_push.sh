6a9f4f87a65d6685b4e42b22aba82023f578a802
Oleksii Avdieiev <oleksii@tech-terrace.org>
2024-08-04 10:02:56 +0200

    feat: Update Docker build and push scripts

---
+++ build_and_push.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+docker build -t docker.io/techterrace/contextdb .
+docker push docker.io/techterrace/contextdb
--- docker_run.sh
+++ docker_run.sh
@@ -2,5 +2,7 @@
 
 docker rm -f contextdb
 
+docker pull techterrace/contextdb
+
 docker run --env-file .env --network host -v chroma:/app/chroma -d --name contextdb --restart always techterrace/contextdb
 
